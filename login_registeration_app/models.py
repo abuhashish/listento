7bc31971fe7395b978ce0df6e7c96743103a7f56
top1million <aasahashish@gmail.com>
2021-06-02 15:30:01 +0300

    registeration models are pushed

--- login_registeration_app/models.py
+++ login_registeration_app/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+import re
+class UserManger(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # add keys and values to errors dictionary for each invalid field
+        if postData['password'] != postData['confirmPass']:
+            errors['notsame']="password doesnt equal confirmation"
+        if len(postData['fname'])<2:
+            errors['short']="fname should be atleast 2 letters"
+        if len(postData['lname'])<2:
+            errors['shorts']="lname should be atleast 2 letters"
+        if len(User.objects.filter(email=postData['email']))!=0:
+            errors['unique']="email is not unqiue" 
+        if len(postData['password']) < 8:
+            errors["desc"] = "password should be at least 8 characters"
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"
+        return errors
+    def login_validator(self,postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your models here.
 class User(models.Model):
