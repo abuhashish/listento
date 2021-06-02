9cc74d2c597339fef62f53278fd3d1ee683c0d59
top1million <aasahashish@gmail.com>
2021-06-02 15:26:27 +0300

    photo done

--- login_registeration_app/models.py
+++ login_registeration_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    image = models.ImageField(upload_to='images/',default='pic.png')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManger() 
